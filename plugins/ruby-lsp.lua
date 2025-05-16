7089819f6f391cdb1bb7c00600501f6a6b25ff68
Caio Rodriguez <caio.rodriguez@datacamp.com>
2025-05-16 21:34:07 +0100

    update with work notebook setup

--- plugins/ruby-lsp.lua
+++ plugins/ruby-lsp.lua
@@ -1,13 +1,37 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        ruby_lsp = {
-          mason = false,
-          cmd = { vim.fn.expand("~/.asdf/shims/ruby-lsp") },
-        },
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      -- ruby_lsp = {
+      --   enabled = true,
+      -- },
+      solargraph = {
+        enabled = true,
+      },
+      rubocop = {
+        -- If Solargraph and Rubocop are both enabled as an LSP,
+        -- diagnostics will be duplicated because Solargraph
+        -- already calls Rubocop if it is installed
+        enabled = false,
+      },
+      standardrb = {
+        enabled = true,
       },
     },
   },
 }
+--
+-- return {
+--   {
+--     "neovim/nvim-lspconfig",
+--     opts = {
+--       servers = {
+--         marksman = {},
+--         ruby_lsp = {
+--           mason = false,
+--           cmd = { vim.fn.expand("~/.asdf/shims/ruby-lsp") },
+--         },
+--       },
+--     },
+--   },
+-- }
